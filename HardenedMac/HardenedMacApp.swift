dcc70e4d532259bb561da21a4850509511dfe863
Dennesssy <139027232+Dennesssy@users.noreply.github.com>
2025-11-09 21:58:53 -0700

    Initial Commit

---
+++ HardenedMac/HardenedMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardenedMacApp.swift
+//  HardenedMac
+//
+//  Created by Dennis Stewart Jr. on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HardenedMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HardenedMacTests/HardenedMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  HardenedMacTests.swift
+//  HardenedMacTests
+//
+//  Created by Dennis Stewart Jr. on 11/9/25.
+//
+
+import Testing
+@testable import HardenedMac
+
+struct HardenedMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
